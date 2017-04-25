4787cd81b548c42ada86b49274921aa9d01e19fa
tanishi <tanishi.success@gmail.com>
2017-04-25 23:57:22 +0900

    update exercise-loops-and-functions.go

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -7,9 +7,10 @@ import (
 
 func Sqrt(x float64) float64 {
 	z := 1.0
+	eps := 1e-10
 
 	for {
-		if math.Abs(z-(z-(math.Pow(z, 2)-x)/(2*z))) <= 1e-10 {
+		if math.Abs(z-(z-(math.Pow(z, 2)-x)/(2*z))) <= eps {
 			break
 		}
 		z = z - (math.Pow(z, 2)-x)/(2*z)
